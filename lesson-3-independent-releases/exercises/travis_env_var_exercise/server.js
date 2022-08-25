19856baedb80fcb7c9e434dded654237d131169a
masayuki-inoue <masayuki.inoue@woven-planet.global>
2022-08-25 15:45:43 +0900

    Insert favorite sport

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,3 +1,4 @@
+const favoriteSport = process.env.SPORT;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,6 +6,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
+    console.log(`${favoriteSport}`);
     console.log("Microservices rock!");
     await sleep(5000);
   }
